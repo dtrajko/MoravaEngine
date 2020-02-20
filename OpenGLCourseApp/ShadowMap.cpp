26936ee3309433b8b9181f297770c386360848d5
Dejan Trajkovic <dtrajko@gmail.com>
2020-02-20 06:15:25 +0100

    31. Directional Shadow Maps (part 1)

---
+++ OpenGLCourseApp/ShadowMap.cpp
@@ -0,0 +1,55 @@
+#include "ShadowMap.h"
+
+
+
+ShadowMap::ShadowMap()
+{
+	FBO = 0;
+	shadowMap = 0;
+}
+
+bool ShadowMap::Init(GLuint width, GLuint height)
+{
+	shadowWidth = width;
+	shadowHeight = height;
+
+	glGenFramebuffers(1, &FBO);
+	glGenTextures(1, &shadowMap);
+	glBindTexture(GL_TEXTURE_2D, shadowMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, shadowWidth, shadowHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, shadowMap, 0);
+
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+
+	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+
+	if (status != GL_FRAMEBUFFER_COMPLETE)
+	{
+		printf("Framebuffer Error: %i\n", status);
+		return false;
+	}
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	return true;
+}
+
+void ShadowMap::Write()
+{
+}
+
+void ShadowMap::Read(GLenum textureUnit)
+{
+}
+
+ShadowMap::~ShadowMap()
+{
+}
---
+++ OpenGLCourseApp/ShadowMap.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdio.h>
+#include <GL/glew.h>
+
+
+class ShadowMap
+{
+public:
+
+	ShadowMap();
+	virtual bool Init(GLuint width, GLuint height);
+	virtual void Write();
+	virtual void Read(GLenum textureUnit);
+	GLuint GetShadowWidth() { return shadowWidth; };
+	GLuint GetShadowHeight() { return shadowHeight; };
+	~ShadowMap();
+
+protected:
+
+	GLuint FBO;
+	GLuint shadowMap;
+	GLuint shadowWidth;
+	GLuint shadowHeight;
+
+
+};
