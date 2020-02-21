83593a09fec45877f284e9bead0a1b95013b1607
Dejan Trajkovic <dtrajko@gmail.com>
2020-02-21 03:41:15 +0100

    Minor adjustments

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -229,7 +229,7 @@ void RenderScene()
 
 	/* Cube Left */
 	model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(-5.0f, 5.0f, -5.0f));
+	model = glm::translate(model, glm::vec3(-5.0f, 3.0f, -5.0f));
 	model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
 	model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 	model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
@@ -241,7 +241,7 @@ void RenderScene()
 
 	/* Cube Right */
 	model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(5.0f, 5.0f, -5.0f));
+	model = glm::translate(model, glm::vec3(5.0f, 3.0f, -5.0f));
 	model = glm::rotate(model, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
 	model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 	model = glm::rotate(model, 0.0f, glm::vec3(0.0f, 0.0f, 1.0f));
