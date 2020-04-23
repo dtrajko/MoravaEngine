f2d72995407997a3d57dbce59404c8d4e60b73ef
Dejan Trajkovic <dtrajko@gmail.com>
2020-04-23 05:26:21 +0200

    Nanosuit model with Light casters

--- OpenGLCourseApp/RendererNanosuit.cpp
+++ OpenGLCourseApp/RendererNanosuit.cpp
@@ -5,9 +5,6 @@
 
 RendererNanosuit::RendererNanosuit()
 {
-	Shader* shaderNanosuit = new Shader("Shaders/nanosuit.vs", "Shaders/nanosuit.fs");
-	shaders.insert(std::make_pair("nanosuit", shaderNanosuit));
-	printf("RendererNanosuit: shaderNanosuit compiled [programID=%d]\n", shaderNanosuit->GetProgramID());
 }
 
 void RendererNanosuit::Init(Scene* scene)
@@ -24,7 +21,14 @@ void RendererNanosuit::SetUniforms()
 
 void RendererNanosuit::SetShaders()
 {
+	Shader* shaderNanosuit = new Shader("Shaders/nanosuit.vs", "Shaders/nanosuit.fs");
 
+	shaderNanosuit->Bind();
+	shaderNanosuit->setInt("material.diffuse", 0);
+	shaderNanosuit->setInt("material.specular", 1);
+
+	shaders.insert(std::make_pair("nanosuit", shaderNanosuit));
+	printf("RendererNanosuit: shaderNanosuit compiled [programID=%d]\n", shaderNanosuit->GetProgramID());
 }
 
 void RendererNanosuit::Render(float deltaTime, Window& mainWindow, Scene* scene, glm::mat4 projectionMatrix)
@@ -42,19 +46,41 @@ void RendererNanosuit::RenderPass(Window& mainWindow, Scene* scene, glm::mat4 pr
 	glClearColor(bgColor.r, bgColor.g, bgColor.b, bgColor.a);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	Shader* shaderNanosuit = shaders["nanosuit"];
+
+	// be sure to activate shader when setting uniforms/drawing objects
+	shaderNanosuit->Bind();
+	shaderNanosuit->setVec3("light.position", scene->GetCamera()->GetPosition());
+	shaderNanosuit->setVec3("light.direction", scene->GetCamera()->GetFront());
+	shaderNanosuit->setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+	shaderNanosuit->setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+	shaderNanosuit->setVec3("viewPos", scene->GetCamera()->GetPosition());
+
+	// light properties
+	shaderNanosuit->setVec3("light.ambient", 2.0f, 2.0f, 2.0f);
+	// we configure the diffuse intensity slightly higher; the right lighting conditions differ with each lighting method and environment.
+	// each environment and lighting type requires some tweaking to get the best out of your environment.
+	shaderNanosuit->setVec3("light.diffuse", 3.0f, 3.0f, 3.0f);
+	shaderNanosuit->setVec3("light.specular", 2.0f, 2.0f, 2.0f);
+	shaderNanosuit->setFloat("light.constant", 1.0f);
+	shaderNanosuit->setFloat("light.linear", 0.09f);
+	shaderNanosuit->setFloat("light.quadratic", 0.032f);
+
+	// material properties
+	shaderNanosuit->setFloat("material.shininess", 32.0f);
+
 	glm::mat4 view = scene->GetCamera()->CalculateViewMatrix();
 
 	// configure transformation matrices
-	shaders["nanosuit"]->Bind();
-	shaders["nanosuit"]->setMat4("projection", projectionMatrix);
-	shaders["nanosuit"]->setMat4("view", view);
+	shaderNanosuit->setMat4("projection", projectionMatrix);
+	shaderNanosuit->setMat4("view", view);
 
 	// Draw the Nanosuit model
 	glm::mat4 model = glm::mat4(1.0f);
 	model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
 	model = glm::rotate(model, glm::radians(modelRotationY), glm::vec3(0.0f, 1.0f, 0.0f));
 	model = glm::scale(model, glm::vec3(1.0f));
-	shaders["nanosuit"]->setMat4("model", model);
+	shaderNanosuit->setMat4("model", model);
 	models["nanosuit"]->Draw(shaders["nanosuit"]);
 
 	std::string passType = "main";
--- OpenGLCourseApp/Shaders/nanosuit.vs
+++ OpenGLCourseApp/Shaders/nanosuit.vs
@@ -4,6 +4,8 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 
+out vec3 FragPos;
+out vec3 Normal;
 out vec2 TexCoords;
 
 uniform mat4 model;
@@ -12,6 +14,8 @@ uniform mat4 projection;
 
 void main()
 {
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(model))) * aNormal;  
     TexCoords = aTexCoords;
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * vec4(FragPos, 1.0);
 }
