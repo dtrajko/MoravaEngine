008ee34fb498bfc71f984427d7e8f9f64dbd748e
Dejan Trajkovic <dtrajko@gmail.com>
2020-06-02 07:32:41 +0200

    Move math constants from main to CommonValues header

--- OpenGLCourseApp/CommonValues.h
+++ OpenGLCourseApp/CommonValues.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #ifndef COMMONVALS
-	#define COMMONVALS
+#define COMMONVALS
 #endif // !COMMONVALS
+
 #include "stb_image.h"
+
+const float M_PI = 3.14159265358979323846f;
+const float toRadians = M_PI / 180.0f;
 
 const int MAX_POINT_LIGHTS = 4;
 const int MAX_SPOT_LIGHTS = 4;
