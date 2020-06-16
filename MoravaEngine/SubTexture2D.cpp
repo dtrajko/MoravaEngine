207494ddcbe775f8771faf986ce1b42e390658a3
Dejan Trajkovic <dtrajko@gmail.com>
2020-06-16 14:30:52 +0200

    Add SubTexture2D from Hazel

---
+++ MoravaEngine/SubTexture2D.cpp
@@ -0,0 +1,30 @@
+#include "SubTexture2D.h"
+
+
+SubTexture2D::SubTexture2D()
+{
+	m_TexCoords[0] = { 0.0f, 0.0f };
+	m_TexCoords[1] = { 0.0f, 0.0f };
+	m_TexCoords[2] = { 0.0f, 0.0f };
+	m_TexCoords[3] = { 0.0f, 0.0f };
+}
+
+SubTexture2D::SubTexture2D(Texture* texture, const glm::vec2& min, const glm::vec2& max)
+	: m_Texture(texture)
+{
+	m_TexCoords[0] = { min.x, min.y };
+	m_TexCoords[1] = { max.x, min.y };
+	m_TexCoords[2] = { max.x, max.y };
+	m_TexCoords[3] = { min.x, max.y };
+}
+
+SubTexture2D* SubTexture2D::CreateFromCoords(Texture* texture, const glm::vec2& coords, const glm::vec2& cellSize, const glm::vec2& spriteSize)
+{
+	glm::vec2 min = { ((coords.x + 0) * cellSize.x) / texture->GetWidth(),  ((coords.y + 0) * cellSize.y) / texture->GetHeight() }; // { 0.0f, 0.0f }
+	glm::vec2 max = { ((coords.x + spriteSize.x) * cellSize.x) / texture->GetWidth(),  ((coords.y + spriteSize.y) * cellSize.y) / texture->GetHeight() }; // { 1.0f, 1.0f }
+	return &SubTexture2D(texture, min, max);
+}
+
+SubTexture2D::~SubTexture2D()
+{
+}
---
+++ MoravaEngine/SubTexture2D.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "Texture.h"
+
+class SubTexture2D
+{
+public:
+	SubTexture2D();
+	SubTexture2D(Texture* texture, const glm::vec2& min, const glm::vec2& max);
+
+	const Texture* GetTexture() const { return m_Texture; };
+	const glm::vec2* GetTexCoords() const { return m_TexCoords; };
+
+	static SubTexture2D* CreateFromCoords(Texture* texture, const glm::vec2& coords, const glm::vec2& cellSize, const glm::vec2& spriteSize);
+	virtual ~SubTexture2D();
+
+private:
+	Texture* m_Texture;
+	glm::vec2 m_TexCoords[4];
+
+};
