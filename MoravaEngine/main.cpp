2baea5dc75145a2b0aee5acd4ac34ed8bf46d7c2
Dejan Trajkovic <dtrajko@gmail.com>
2020-08-17 02:54:39 +0200

    Project build automation with CMake (working version Windows 64-bit)

--- MoravaEngine/main.cpp
+++ MoravaEngine/main.cpp
@@ -34,7 +34,7 @@
 #include "SceneTerrain.h"
 #include "ScenePBR.h"
 #include "SceneJoey.h"
-#include "SourceConditional/SceneBullet.h"
+#include "SceneBullet.h"
 #include "SceneInstanced.h"
 #include "SceneAsteroids.h"
 #include "SceneNanosuit.h"
@@ -95,7 +95,7 @@ enum class SceneName
 	MarchingCubes,
 };
 
-SceneName currentScene = SceneName::MarchingCubes;
+SceneName currentScene = SceneName::Bullet;
 
 // Key cooldown time (emulate onKeyReleased)
 EventCooldown keyPressCooldown = { 0.0f, 0.2f };
