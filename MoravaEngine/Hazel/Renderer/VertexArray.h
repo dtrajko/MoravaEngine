aece6fe099e26a4bb195d33164c49da1848951cb
Dejan Trajkovic <dtrajko@gmail.com>
2020-10-04 18:12:59 +0200

    Implementing an environment map scene (PBR/IBL) (part 9)

--- MoravaEngine/Hazel/Renderer/VertexArray.h
+++ MoravaEngine/Hazel/Renderer/VertexArray.h
@@ -14,11 +14,11 @@ namespace Hazel {
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 
-		virtual void AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) = 0;
-		virtual void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) = 0;
+		virtual void AddVertexBuffer(VertexBuffer* vertexBuffer) = 0;
+		virtual void SetIndexBuffer(IndexBuffer* indexBuffer) = 0;
 
-		virtual const std::vector<std::shared_ptr<VertexBuffer>>& GetVertexBuffers() const = 0;
-		virtual const std::shared_ptr<IndexBuffer>& GetIndexBuffer() const = 0;
+		virtual const std::vector<VertexBuffer*>& GetVertexBuffers() const = 0;
+		virtual const IndexBuffer& GetIndexBuffer() const = 0;
 
 		virtual uint32_t GetID() const = 0;
 
