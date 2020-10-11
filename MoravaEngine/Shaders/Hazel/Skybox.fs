10b104b1f177b6da244e6394fbc7391f7c3e8ff9
Dejan Trajkovic <dtrajko@gmail.com>
2020-10-11 10:59:05 +0200

    Apply exposure to skybox shader

--- MoravaEngine/Shaders/Hazel/Skybox.fs
+++ MoravaEngine/Shaders/Hazel/Skybox.fs
@@ -6,10 +6,12 @@ layout(location = 0) out vec4 finalColor;
 
 uniform samplerCube u_Texture;
 uniform float u_TextureLod;
+uniform float u_Exposure;
 
 in vec3 v_Position;
 
 void main()
 {
 	finalColor = textureLod(u_Texture, v_Position, u_TextureLod);
+	finalColor.rgb *= u_Exposure;
 }
