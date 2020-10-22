1134158ae7b53fabe67f66299dc41600f2f73231
Dejan Trajkovic <dtrajko@gmail.com>
2020-10-22 15:03:37 +0200

    New class HazelRenderer

---
+++ MoravaEngine/Hazel/Renderer/HazelRenderer.cpp
@@ -0,0 +1,19 @@
+#include "HazelRenderer.h"
+
+#include "../../Log.h"
+
+
+namespace Hazel {
+
+	void HazelRenderer::Clear()
+	{
+	}
+
+	void HazelRenderer::Clear(float r, float g, float b, float a)
+	{
+	}
+
+	void HazelRenderer::SetClearColor(float r, float g, float b, float a)
+	{
+	}
+}
---
+++ MoravaEngine/Hazel/Renderer/HazelRenderer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+namespace Hazel {
+
+	class HazelRenderer
+	{
+	public:
+		typedef void(*RenderCommandFn)(void*);
+
+		// Commands
+		static void Clear();
+		static void Clear(float r, float g, float b, float a = 1.0f);
+		static void SetClearColor(float r, float g, float b, float a);
+
+		// TODO remaining methods ...
+
+	};
+
+}
