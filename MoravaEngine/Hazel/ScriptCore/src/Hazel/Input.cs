92a8d2fc8fbefea2b7481ca27beadb1854236e0c
Dejan Trajkovic <dtrajko@gmail.com>
2021-01-04 03:52:47 +0100

    ScriptCore C# classes included

---
+++ MoravaEngine/Hazel/ScriptCore/src/Hazel/Input.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazel
+{
+    public class Input
+    {
+
+        public static bool IsKeyPressed(KeyCode keycode)
+        {
+            return IsKeyPressed_Native(keycode);
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern bool IsKeyPressed_Native(KeyCode keycode);
+
+    }
+}
---
+++ MoravaEngine/Hazel/ScriptCore/src/Hazel/Math/Vector4.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazel
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public struct Vector4
+    {
+        [FieldOffset(0)] public float X;
+        [FieldOffset(4)] public float Y;
+        [FieldOffset(8)] public float Z;
+        [FieldOffset(12)] public float W;
+
+        public Vector4(float scalar)
+        {
+            X = Y = Z = W =scalar;
+        }
+
+        public Vector4(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+        public static Vector4 operator+(Vector4 left, Vector4 right)
+        {
+            return new Vector4(left.X + right.X, left.Y + right.Y, left.Z + right.Z, left.W + right.W);
+        }
+
+        public static Vector4 operator-(Vector4 left, Vector4 right)
+        {
+            return new Vector4(left.X - right.X, left.Y - right.Y, left.Z - right.Z, left.W - right.W);
+        }
+
+        public static Vector4 operator*(Vector4 left, Vector4 right)
+        {
+            return new Vector4(left.X * right.X, left.Y * right.Y, left.Z * right.Z, left.W * right.W);
+        }
+
+        public static Vector4 operator *(Vector4 left, float scalar)
+        {
+            return new Vector4(left.X * scalar, left.Y * scalar, left.Z * scalar, left.W * scalar);
+        }
+
+        public static Vector4 operator *(float scalar, Vector4 right)
+        {
+            return new Vector4(scalar * right.X, scalar * right.Y, scalar * right.Z, scalar * right.W);
+        }
+
+        public static Vector4 operator/(Vector4 left, Vector4 right)
+        {
+            return new Vector4(left.X / right.X, left.Y / right.Y, left.Z / right.Z, left.W / right.W);
+        }
+
+        public static Vector4 operator /(Vector4 left, float scalar)
+        {
+            return new Vector4(left.X / scalar, left.Y / scalar, left.Z / scalar, left.W / scalar);
+        }
+
+        public static Vector4 Lerp(Vector4 a, Vector4 b, float t)
+        {
+            if (t < 0.0f) t = 0.0f;
+            if (t > 1.0f) t = 1.0f;
+            return (1.0f - t) * a + t * b;
+        }
+
+    }
+}
