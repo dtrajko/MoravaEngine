4189a60c3438e8071d71cabd33fc81de08b65ad5
Dejan Trajkovic <dtrajko@gmail.com>
2021-02-11 14:59:42 +0100

    Trying to enable Renderer::Submit in OpenGLIndexBuffer

--- MoravaEngine/Hazel/Platform/OpenGL/OpenGLIndexBuffer.cpp
+++ MoravaEngine/Hazel/Platform/OpenGL/OpenGLIndexBuffer.cpp
@@ -1,6 +1,7 @@
 #include "OpenGLIndexBuffer.h"
 
 #include "../../Core/Assert.h"
+#include "../../Renderer/HazelRenderer.h"
 
 #include <gl/glew.h>
 
@@ -17,14 +18,11 @@ namespace Hazel {
 	{
 		m_LocalData = Buffer::Copy(data, size);
 
-		//	Ref<OpenGLIndexBuffer> instance = this;
-		//	Renderer::Submit([instance]() mutable {
-		//		glCreateBuffers(1, &instance->m_RendererID);
-		//		glNamedBufferData(instance->m_RendererID, instance->m_Size, instance->m_LocalData.Data, GL_STATIC_DRAW);
-		//	});
-
-		glCreateBuffers(1, &m_RendererID);
-		glNamedBufferData(m_RendererID, m_Size, m_LocalData.Data, GL_STATIC_DRAW);
+		Ref<OpenGLIndexBuffer> instance = this;
+		HazelRenderer::Submit([instance]() mutable {
+			glCreateBuffers(1, &instance->m_RendererID);
+			glNamedBufferData(instance->m_RendererID, instance->m_Size, instance->m_LocalData.Data, GL_STATIC_DRAW);
+		});
 	}
 
 	OpenGLIndexBuffer::OpenGLIndexBuffer(uint32_t size)
@@ -32,31 +30,37 @@ namespace Hazel {
 	{
 		// m_LocalData = Buffer(size);
 
-		//	Ref<OpenGLIndexBuffer> instance = this;
-		//	Renderer::Submit([instance]() mutable {
-		//		glCreateBuffers(1, &instance->m_RendererID);
-		//		glNamedBufferData(instance->m_RendererID, instance->m_Size, nullptr, GL_DYNAMIC_DRAW);
-		//	});
-
-		glCreateBuffers(1, &m_RendererID);
-		glNamedBufferData(m_RendererID, m_Size, nullptr, GL_DYNAMIC_DRAW);
+		Ref<OpenGLIndexBuffer> instance = this;
+		HazelRenderer::Submit([instance]() mutable {
+			glCreateBuffers(1, &instance->m_RendererID);
+			glNamedBufferData(instance->m_RendererID, instance->m_Size, nullptr, GL_DYNAMIC_DRAW);
+		});
 	}
 
 	OpenGLIndexBuffer::~OpenGLIndexBuffer()
 	{
-		glDeleteBuffers(1, &m_RendererID);
+		GLuint rendererID = m_RendererID;
+		HazelRenderer::Submit([rendererID]() {
+			glDeleteBuffers(1, &rendererID);
+		});
 	}
 
 	void OpenGLIndexBuffer::SetData(void* data, uint32_t size, uint32_t offset)
 	{
 		m_LocalData = Buffer::Copy(data, size);
 		m_Size = size;
-		glNamedBufferSubData(m_RendererID, offset, m_Size, m_LocalData.Data);
+		Ref<OpenGLIndexBuffer> instance = this;
+		HazelRenderer::Submit([instance, offset](){
+			glNamedBufferSubData(instance->m_RendererID, offset, instance->m_Size, instance->m_LocalData.Data);
+		});
 	}
 
 	void OpenGLIndexBuffer::Bind() const
 	{
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		Ref<const OpenGLIndexBuffer> instance = this;
+		HazelRenderer::Submit([instance]() {
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, instance->m_RendererID);
+		});
 	}
 
 }
