79a1b694120f238ae429261e76d54decbebf6a32
Dejan Trajkovic <dtrajko@gmail.com>
2021-10-12 02:14:53 +0200

    EditorLayerVulkan implementation (4)

--- MoravaEngine/src/Core/Input.h
+++ MoravaEngine/src/Core/Input.h
@@ -6,6 +6,32 @@
 #include <utility>
 
 
+enum class CursorMode
+{
+	Normal = 0,
+	Hidden = 1,
+	Locked = 2
+};
+
+typedef enum class MouseButton : uint16_t
+{
+	Button0 = 0,
+	Button1 = 1,
+	Button2 = 2,
+	Button3 = 3,
+	Button4 = 4,
+	Button5 = 5,
+	Left = Button0,
+	Right = Button1,
+	Middle = Button2
+} Button;
+
+inline std::ostream& operator<<(std::ostream& os, MouseButton button)
+{
+	os << static_cast<int32_t>(button);
+	return os;
+}
+
 class Input
 {
 public:
@@ -15,5 +41,8 @@ public:
 	static std::pair<float, float> GetMousePosition();
 	static float GetMouseX();
 	static float GetMouseY();
+
+	static void SetCursorMode(CursorMode mode);
+	static CursorMode GetCursorMode();
 
 };
