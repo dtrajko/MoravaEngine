31ac0e7ff4b069d64d179db5f5820b432b5f86a6
Dejan Trajkovic <dtrajko@gmail.com>
2022-01-09 20:04:16 +0100

    Fix error in config.h

--- MoravaEngine/src/config.h
+++ MoravaEngine/src/config.h
@@ -27,7 +27,7 @@
 // #define SCENE_EDITOR
 // #define SCENE_EDITOR_IMGUIZMO
 // #define SCENE_HAZEL_ENV_MAP
-#define SCENE_DX11
+// #define SCENE_DX11
 // #define SCENE_HAZEL_VULKAN
 // #define SCENE_ENV_MAP_VULKAN
 
